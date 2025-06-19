5beb241ec019e7ba082676f6bdca0df5a6ed2f8d
James Wainwright <james.wainwright@lowrisc.org>
2025-06-19 11:41:15 +0100

    [test] Print asserted alerts caught by OTTF
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/device/lib/testing/test_framework/ottf_isrs.c
+++ sw/device/lib/testing/test_framework/ottf_isrs.c
@@ -6,6 +6,7 @@
 
 #include "sw/device/lib/base/csr.h"
 #include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/dif/dif_alert_handler.h"
 #include "sw/device/lib/dif/dif_rv_plic.h"
 #include "sw/device/lib/runtime/hart.h"
 #include "sw/device/lib/runtime/ibex.h"
@@ -14,6 +15,7 @@
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_test_config.h"
 
+#include "alert_handler_regs.h"
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 dif_rv_plic_t ottf_plic;
@@ -195,6 +197,22 @@ bool ottf_console_flow_control_isr(uint32_t *exc_info) { return false; }
 
 OT_WEAK
 bool ottf_alert_isr(uint32_t *exc_info) {
+  dif_alert_handler_t alert_handler;
+  CHECK_DIF_OK(dif_alert_handler_init(
+      mmio_region_from_addr(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR),
+      &alert_handler));
+
+  // Log all asserted alerts.
+  for (dif_alert_handler_alert_t alert = 0;
+       alert < ALERT_HANDLER_PARAM_N_ALERTS; alert++) {
+    bool is_cause = false;
+    CHECK_DIF_OK(
+        dif_alert_handler_alert_is_cause(&alert_handler, alert, &is_cause));
+    if (is_cause) {
+      LOG_ERROR("INFO: Alert %d is asserted", alert);
+    }
+  }
+
   ottf_generic_fault_print(exc_info, "Alert IRQ", ibex_mcause_read());
   abort();
 }
